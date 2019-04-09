db99967b07c855ec078abf9be51e47f3568f77d7
Arjun <arjunsekar1991@gmail.com>
2019-04-08 22:32:28 -0400

    fixed document object

--- newsgroup.py
+++ newsgroup.py
@@ -3,39 +3,49 @@ processing the special format used by the Cranfield Dataset
 '''
 from doc import Document
 import os
+import glob
+from fnmatch import fnmatch
 
 class NewsGroup:
-    def __init__(self, filename):
+    def __init__(self, files):
         self.docs = []
+        for filename in files:
+            newsGroupFile = open(filename)
+            head, tail = os.path.split(filename)
+            docid = tail
+            subject = ''
+            message = ''
+            startread = False
+            buf = ''
+            for line in newsGroupFile:
+    #            print (line)
 
-        newsGroupFile = open(filename)
-        head, tail = os.path.split(filename)
-        docid = tail
-        subject = ''
-        message = ''
-        startread = False
-        buf = ''
-        for line in newsGroupFile:
-#            print (line)
-
-            if 'Subject:' in line:
-                subject = line[9:] # got title
-            elif 'Lines:' in line:
-                startread = True
-                line=''
-            if startread:
-                  buf += line
+                if 'Subject:' in line:
+                    subject = line[9:] # got title
+                elif 'Lines:' in line:
+                    startread = True
+                    line=''
+                if startread:
+                      buf += line
 
 
-        message=buf;
-        self.docs.append(Document(docid, subject, message)) # the last one
+            message=buf;
+            self.docs.append(Document(docid, subject, message)) # the last one
 
 if __name__ == '__main__':
     ''' testing '''
+    cwd = os.getcwd()+"\\mini_newsgroups"
+    #print(glob.glob(cwd+"/mini_newsgroups/*.FILE"))
+    dataFileslist = []
 
-    newsGroupFile = NewsGroup ('C:\\Users\\arjun\\Documents\\Github\\textclassifier\\textclassification\\mini_newsgroups\\alt.atheism\\51121')
-    for doc in newsGroupFile.docs:
-        print(doc.docID)
-        print(doc.subject)
-        print(doc.message)
+    for path, subdirs, files in os.walk(cwd):
+        for name in files:
+           # if fnmatch(name, pattern):
+            print(os.path.join(path, name))
+            dataFileslist.append(os.path.join(path, name))
+    newsGroupFile = NewsGroup (dataFileslist)
+   # for doc in newsGroupFile.docs:
+    #    print(doc.docID)
+     #   print(doc.subject)
+      #  print(doc.message)
     print(len(newsGroupFile.docs))
